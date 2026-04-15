5a7543ab502465a3d180bcaac5e59b2e5e0354ee
JiuLiutop <3209174373@qq.com>
2026-04-16 00:21:06 +0800

    layout

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,35 @@
 <template>
-<Header />
+        <a-layout :style="{ height: '100vh' }">
+                <Header/>
+                <a-layout>
+                        <a-layout-sider :style="siderStyle">Sider</a-layout-sider>
+                        <a-layout-content :style="contentStyle">Content</a-layout-content>
+                </a-layout>
+                <a-layout-footer :style="footerStyle">v0.1</a-layout-footer>
+        </a-layout>
 </template>
 
-<script setup lang="ts">
-import Header from "./components/header.vue";
+<script lang="ts" setup>
+import type { CSSProperties } from 'vue';
+import Header from "./components/header.vue"
+const contentStyle: CSSProperties = {
+        textAlign: 'center',
+        color: '#fff',
+        backgroundColor: '#222222',
+        minHeight: '0',
+        flex: '1 1 auto',
+};
+
+const siderStyle: CSSProperties = {
+        textAlign: 'center',
+        color: '#fff',
+        backgroundColor: '#373737',
+};
+
+const footerStyle: CSSProperties = {
+        color: '#fff',
+        backgroundColor: '#2276ae',
+        padding: '4px 20px',
+};
 </script>
 
-<style>
-body{
-        background-color: #222222;
-}
-</style>
--- src/components/header.vue
+++ src/components/header.vue
@@ -1,10 +1,15 @@
 <template>
-        <div class="py-2 bg-white w-full">
-                123
-        </div>
-
+        <a-layout-header :style="headerStyle">Header</a-layout-header>
 </template>
 <script setup lang="ts">
+import type { CSSProperties } from 'vue';
+const headerStyle: CSSProperties = {
+        color: '#fff',
+        height: 64,
+        paddingInline: 50,
+        lineHeight: '64px',
+        backgroundColor: 'rgb(43,43,43)',
+};
 
 </script>
 <style scoped>
